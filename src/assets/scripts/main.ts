cebf8f932edc480343e0d3d87365d9907b711526
TheChiran <tonmoychiran449@gmail.com>
2022-08-13 11:29:46 +0600

    [Task-> 1. initialized html structure]

--- src/assets/scripts/main.ts
+++ src/assets/scripts/main.ts
@@ -1,5 +1,20 @@
+function buttonEventListener(){
+    const buttonElement = document.getElementById('ham') !;
+    const callback = (): void=>{
+        if(buttonElement !== null){
+            buttonElement.classList.add('remove');
+        }
+    }
+
+    buttonElement.addEventListener('click',callback);
+}
+
+function eventListeners(){
+    buttonEventListener();
+}
+
 export default function main(){
-    console.log('hello world')
+    eventListeners();
 }
 
 main();
